68eb55ac33e3ac4a7625c53d7c4140ac1c21cb7c
caio meirelles <cvitormeirelles@gmail.com>
2025-12-20 11:29:55 -0300

    test(ui): add jest setup and fix jsdom env

--- ui/jest.config.cjs
+++ ui/jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/__tests__/**/*.test.(ts|tsx|js)'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
   transform: {
     '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
---
+++ ui/jest.setup.js
@@ -0,0 +1,3 @@
+// Jest setup for ui tests
+require('@testing-library/jest-dom');
+// add any global mocks or polyfills here
