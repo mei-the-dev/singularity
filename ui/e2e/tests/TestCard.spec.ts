999ddee00e1b0de9aa271b92e1efb65fead0a13b
caio meirelles <cvitormeirelles@gmail.com>
2025-12-21 13:12:03 -0300

    clean up

--- ui/e2e/tests/TestCard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('TestCard renders correctly', async ({ page }) => {
-  await page.goto('http://localhost:6006/iframe.html?id=test-testcard--default');
-  await page.waitForSelector('[class*="rounded-xl"]');
-  await expect(page).toHaveScreenshot('testcard-default.png');
-});
